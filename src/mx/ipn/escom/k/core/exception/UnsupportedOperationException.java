057539e052989a24b838c1e4f21b3490d1136789
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2025-04-30 04:17:44 -0600

    Delete UnsupportedOperationException.java

--- src/mx/ipn/escom/k/core/exception/UnsupportedOperationException.java
+++
@@ -1,7 +0,0 @@
-package mx.ipn.escom.k.core.exception;
-
-public class UnsupportedOperationException extends RuntimeException {
-    public UnsupportedOperationException() {
-        super("Not implemented yet");
-    }
-}
